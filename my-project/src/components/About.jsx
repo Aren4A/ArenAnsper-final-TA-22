5bcf864b087fdb9f9428d4e5716cbf66fa7471a4
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2024-03-06 16:37:49 +0200

    update

--- my-project/src/components/About.jsx
+++ my-project/src/components/About.jsx
@@ -4,8 +4,28 @@ import React from 'react';
 function About() {
   return (
     <section id="about">
-      <h2>About Us</h2>
-      <p>This is the about section...</p>
+      <div
+  class="flex flex-col px-20 py-12 text-3xl text-justify text-blue-900 bg-yellow-400"
+>
+  <div
+    class="flex gap-5 justify-between mt-9"
+  >
+    <div class="flex-auto self-end mt-10 italic">
+      Kamur Finance OÜ pooldab paberivaba raamatupidamist - eelkõige
+      teeb see aruandluse kajastamise mugavamaks, kiiremaks ja
+      ajakohasemaks. Meie eesmärk on julgustada alustavate
+      mikroettevõtete ideede elluviimist nii, et loomingulised ideed ei
+      jääks ellu viimata tüütu raamatupidamise kohustuse pärast, millege
+      ise ei jõua või ei soovi esmajärjekorras tegeleda.
+    </div>
+    <img src="/src/assets/syda1.png"
+      class="self-start aspect-[1.03] w-[73px]"
+    />
+  </div>
+  <img src="/src/assets/syda2.png"
+    class="mt-8 self-start aspect-[1.03] w-[73px] "
+  />
+</div>
     </section>
   );
 }
--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -4,17 +4,16 @@ import backgroundImage from '../assets/cropped_ananass.jpg';
 
 function Header() {
   return (
-    <div className="bg-cover bg-center h-" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <header className='flex justify-end '>
-        <a href="/link1" className="text-amber-400 mr-4">Visioon</a>
-        <a href="/link2" className="text-amber-400 mr-4">Hind</a>
-        <a href="/link3" className="text-amber-400 mr-4">Kontakt</a>
+    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <header className='flex justify-end p-5'>
+        <a href="/link1" className="text-amber-400 mr-7 text-3xl">Visioon</a>
+        <a href="/link2" className="text-amber-400 mr-7 text-3xl">Hind</a>
+        <a href="/link3" className="text-amber-400 mr-5 text-3xl">Kontakt</a>
       </header>
-    <div>
-      <h1>Kamur Finance OÜ</h1>
+      <div>
+        <h1 className='text-amber-400 text-8xl p-7 mt-24'>Kamur Finance OÜ</h1>
+      </div>
     </div>
-  </div>
-    
   );
 }
 
