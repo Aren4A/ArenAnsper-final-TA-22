932cfde539278e562e64ff6c7504b08df695dc1b
Aren4A <aren.ansper@gmail.com>
2024-03-07 11:38:28 +0200

    textarea

--- my-project/src/components/Contact.jsx
+++ my-project/src/components/Contact.jsx
@@ -13,37 +13,32 @@ function Contact() {
     <div class="flex flex-col">
             <div class="flex flex-col mb-3">
               <label class="form-label">Eesnimi</label>
-              <input name="first_name" type="text" class="form-control lg:w-80 rounded-xl" required />
+              <input name="first_name" type="text" className="input input-bordered w-full max-w-xs" required />
             </div>
 
             <div class="flex flex-col mb-3">
               <label class="form-label">Perekonnanimi</label>
-              <input name="last_name" type="text" class="form-control lg:w-80 rounded-xl" required />
+              <input name="last_name" type="text" className="input input-bordered w-full max-w-xs" required />
             </div>
 
             <div class="flex flex-col mb-3">
               <label class="form-label">Ettevõtte nimi</label>
-              <input name="company_name" type="text" class="form-control lg:w-80 rounded-xl" required />
+              <input name="company_name" type="text" className="input input-bordered w-full max-w-xs" required />
             </div>
 
             <div class="flex flex-col mb-3">
               <label class="form-label">Email</label>
-              <input name="email" type="email" class="form-control lg:w-80 rounded-xl" required />
+              <input name="email" type="email" className="input input-bordered w-full max-w-xs" required />
             </div>
 
             <div class="flex flex-col mb-3">
               <label class="form-label">Telefon</label>
-              <input name="phone" type="tel" class="form-control lg:w-80 rounded-xl" />
+              <input name="phone" type="tel" className="input input-bordered w-full max-w-xs" required />
             </div>
 
             <div class="flex flex-col mb-3 w-1/2">
               <label class="form-label">Lisainfo</label>
-              <textarea
-                name="message"
-                class="form-control w-full rounded-xl"
-                rows="10"
-                required
-              ></textarea>
+              <textarea name="message" className="textarea textarea-bordered" rows="10"></textarea>
             </div>
             
   </div>
