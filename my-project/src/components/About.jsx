98f64f9e528b6555fa578e31faa5f84d9f465493
Aren4A <aren.ansper@gmail.com>
2024-03-19 21:22:59 +0200

    text

--- my-project/src/components/About.jsx
+++ my-project/src/components/About.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 function About() {
   return (
     <section id="visioon">
-      <div class="flex flex-col lg:px-20 px-4 py-12 text-3xl text-justify text-[#012579] bg-[#E3C10C]">
+      <div class="flex flex-col lg:px-20 px-4 py-12 text-xl lg:text-3xl text-justify text-[#012579] bg-[#E3C10C]">
         <div class="flex flex-col lg:flex-row-reverse gap-5 justify-between my-9">
           <img src="/syda2.png" class="self-start aspect-[1.03] lg:w-[50px] w-20" />  
-          <div class="px-2 text-center flex-auto self-end my-10 italic">
+          <div class="px-2 text-center flex-auto self-end lg:my-10 italic">
             Kamur Finance OÜ pooldab paberivaba raamatupidamist - eelkõige
             teeb see aruandluse kajastamise mugavamaks, kiiremaks ja
             ajakohasemaks. Meie eesmärk on julgustada alustavate
