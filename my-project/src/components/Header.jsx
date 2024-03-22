288ad5ef6b93b1401b1e156a7adef3f7aef23bc3
Aren4A <aren.ansper@gmail.com>
2024-03-22 09:52:11 +0200

    s

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
         <a href="#kontakt" className="text-[#E3C10C] mr-5 text-3xl">Kontakt</a>
       </header>
       <div className='flex flex-col justify-center'>
-        <h1 className='dancing-script lg:pl-20 text-[#E3C10C] [font-size:_clamp(3.5em,3vw,10em)] px-3 leading-[50px]'>Raamatupidamine mikro- ja <br />väikeettevõtetele</h1>
+        <h1 className='dancing-script lg:pl-20 text-[#E3C10C] [font-size:_clamp(4em,3vw,10em)] px-3 leading-[60px]'>Raamatupidamine mikro- ja <br />väikeettevõtetele</h1>
         <h1 className='text-black lg:pl-20 text-lg pr-[45%] pl-3 pt-3'>“If you think it's expensive to hire a professional to do the job, wait until you hire an amateur.”</h1>
         <h1 className='text-black lg:pl-20 text-lg pr-[45%] pl-3 pt-3'>-Red Adair</h1>
       </div>
