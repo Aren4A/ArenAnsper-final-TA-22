b7f3ad161e8dd9df7d2b58122b334012533f1be7
Aren4A <aren.ansper@gmail.com>
2024-05-04 07:57:36 +0300

    refac

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
 import About from './components/About';
 import Contactform from './components/Contactform';
 import Footer from './components/Footer';
