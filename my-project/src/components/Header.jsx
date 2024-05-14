4246cad8624ffbd876555031621e00f979403d71
Aren4A <aren.ansper@gmail.com>
2024-05-14 20:41:05 +0300

    w-fit

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -26,7 +26,7 @@ function Header() {
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden justify-start">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-fit">
         <li>
           <ul className="p-2">
           <li><a><Link to="/extra" className="text-black">{t('headerExtraLinks', { appName: "App" })}</Link></a></li>
